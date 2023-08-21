c5f27ffb8c76899d77303090efca9a28d5190887
Henrik Greve <Henrik.Greve@bksv.com>
2023-08-21 16:06:24 +0200

    Map showing Inner City as initial region

--- GoEasy/components/Map.tsx
+++ GoEasy/components/Map.tsx
@@ -1,10 +1,21 @@
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export const Map = () => {
   const insets = useSafeAreaInsets();
+
+  //defining a state variable inner city copenhagen as fallback
+  const [initialRegion, setInitialRegion] = useState({
+    coords: {
+      latitude: 55.68375507989918,
+      longitude: 12.57572185309855,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  });
+
   return (
     <View
       style={[
@@ -19,6 +30,7 @@ export const Map = () => {
     >
       <MapView
         style={styles.map}
+        initialRegion={initialRegion.coords}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         showsMyLocationButton={false}
@@ -31,7 +43,6 @@ export const Map = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "transparent",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
