20bdd7877670e1afe157434155ffdc17b102adae
Henrik Greve <Henrik.Greve@bksv.com>
2023-08-21 20:57:50 +0200

    permisson working

--- GoEasy/components/Map.tsx
+++ GoEasy/components/Map.tsx
@@ -1,11 +1,12 @@
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { StyleSheet, View, Text } from "react-native";
-import React, { useState } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import * as Location from "expo-location";
 
 export const Map = () => {
   const insets = useSafeAreaInsets();
-
+  let mapRef = useRef<MapView | null>(null);
   //defining a state variable inner city copenhagen as fallback
   const [initialRegion, setInitialRegion] = useState({
     coords: {
@@ -15,6 +16,71 @@ export const Map = () => {
       longitudeDelta: 0.0421,
     },
   });
+
+  // Request permissions right after starting the app
+  useEffect(() => {
+    const requestPermissions = async () => {
+      const foreground = await Location.requestForegroundPermissionsAsync();
+      if (foreground.granted) startForegroundUpdate();
+    };
+    requestPermissions();
+  }, []);
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.animateToRegion({
+        latitude: initialRegion.coords.latitude,
+        longitude: initialRegion.coords.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      });
+    }
+  }, [initialRegion]);
+
+  // Start location tracking in foreground
+  const startForegroundUpdate = async () => {
+    // Check if foreground permission is granted
+    const { granted } = await Location.getForegroundPermissionsAsync();
+    if (!granted) {
+      console.log("location tracking denied");
+      return;
+    }
+    let foregroundSubscription: any;
+    // Make sure that foreground location tracking is not running
+    foregroundSubscription?.remove();
+
+    // Start watching position in real-time
+    foregroundSubscription = await Location.watchPositionAsync(
+      {
+        // For better logs, we set the accuracy to the most sensitive option
+        accuracy: Location.Accuracy.BestForNavigation,
+      },
+      (location) => {
+        try {
+          if (location.coords === null) {
+            // Handle invalid location data, if needed
+            console.log("Invalid location data received.");
+            return;
+          }
+
+          const locationRegion = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          };
+
+          console.log(location.coords.latitude);
+          setInitialRegion((prevRegion) => ({
+            ...prevRegion,
+            coords: locationRegion,
+          }));
+        } catch (error) {
+          console.error("Error while processing location:", error);
+          // Handle the error gracefully, show an error message, or take appropriate actions
+        }
+      }
+    );
+  };
 
   return (
     <View
@@ -29,6 +95,7 @@ export const Map = () => {
       ]}
     >
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={initialRegion.coords}
         provider={PROVIDER_GOOGLE}
